434e27591108f9f49dee2a0db8741c34bf5bddc5
Barbara Grochalska <b.grochalska@gmail.com>
2018-09-22 10:47:40 +0200

    types conversion

--- main.js
+++ main.js
@@ -1,28 +1,34 @@
-//    alert('hello world');
+/*var num = 1; //literal
+var foo = 'foo';
+var foo2 = String('foo2');
+var num2 = Number(1); //dwie możliwości nadawania number, tutaj jest funkcja konstruktora
+var b = "123";
+//var num2 = Number(b); //ze stringa zrobi number, tz. konwersja typów
 
-//var a = "hello types";
-//console.log('type is:', typeof a);
+//console.log(num, num2);
 
-//var a = 123;
-//console.log(typeof a);
+//console.log(foo,foo2);
+console.log(Number(b));*/
 
-/*var a = [1];
-var b = a;
-b.push(2);
-
-console.log(a,b);*/
-
-var a =1;
-var b = 2;
- console.log('definition:', a,b);
-
- var c = a+b;
- console.log('assigment:', c);
- 
+var num =1;
+var stringNum = "2";
+var stringNotNumber = 'hello';
+console.log(num + Number(stringNotNumber)); // will return NaN
 
 
+var constructorNum = Number(stringNum);
+console.log(num + stringNum); //12
+console.log(num + Number(stringNum)); //3
+console.log(num + constructorNum);
 
 
+var array1 = [];
+var array2 = [];
+
+console.log(array1+array2); //zwraca pusty string*/
+
+//{}+[]; // 0
+//+[] //0
 
 
 
