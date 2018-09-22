e2207211e7a314a7f4afd3986ef14e40f151e1fe
Barbara Grochalska <b.grochalska@gmail.com>
2018-09-22 11:06:53 +0200

    var const let

--- main.js
+++ main.js
@@ -1,36 +1,14 @@
-/*var num = 1; //literal
-var foo = 'foo';
-var foo2 = String('foo2');
-var num2 = Number(1); //dwie możliwości nadawania number, tutaj jest funkcja konstruktora
-var b = "123";
-//var num2 = Number(b); //ze stringa zrobi number, tz. konwersja typów
+//const i let - stałe
 
-//console.log(num, num2);
+//ES5 only;
+var a = 'foo'; //nadpisanie mozliwe1
 
-//console.log(foo,foo2);
-console.log(Number(b));*/
+//ES6 only;
+const BAR = "I'm bar"; //nadpisanie powinno byc niemożliwe
 
-var num =1;
-var stringNum = "2";
-var stringNotNumber = 'hello';
-console.log(num + Number(stringNotNumber)); // will return NaN
+let baz = "boo"; //let moze byc nadpisany - nie mozna go zadeklarowac ponownie let baz="goo"; blokowy
+baz = "booze";
 
-
-var constructorNum = Number(stringNum);
-console.log(num + stringNum); //12
-console.log(num + Number(stringNum)); //3
-console.log(num + constructorNum);
-
-
-var array1 = [];
-var array2 = [];
-
-console.log(array1+array2); //zwraca pusty string*/
-
-//{}+[]; // 0
-//+[] //0
-
-
-
+console.log(baz);
 
 
