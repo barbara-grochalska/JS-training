8337c382163694e51035700040a8b7356cbdedfd
Barbara Grochalska <b.grochalska@gmail.com>
2018-09-22 09:54:35 +0200

    var definition/assigment

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
 //    alert('hello world');
-var a = "hello types";
-console.log('type is:', typeof a);
+
+//var a = "hello types";
+//console.log('type is:', typeof a);
+
+//var a = 123;
+//console.log(typeof a);
+
+/*var a = [1];
+var b = a;
+b.push(2);
+
+console.log(a,b);*/
+
+var a =1;
+var b = 2;
+ console.log('definition:', a,b);
+
+ var c = a+b;
+ console.log('assigment:', c);
+ 
+
+
+
+
+
+
 
 
 
