213b9139d88d64b59f5f759dc8db1953e3569b3a
Barbara Grochalska <b.grochalska@gmail.com>
2018-09-29 10:49:25 +0200

    push pop unshift shift slice

--- day3/main.js
+++ day3/main.js
@@ -17,17 +17,17 @@
 
 // console.timeEnd ('before loop');
 
-var manyNumbers = [];
+// var manyNumbers = [];
 
-for(let i=0; i<10; i++){
-    const myNumbers = (Math.floor(Math.random() * 1000)+1);
-    manyNumbers.push(myNumbers);
-        if (i>1){
-        break;
-   }
+// for(let i=0; i<10; i++){
+//     const myNumbers = (Math.floor(Math.random() * 1000)+1);
+//     manyNumbers.push(myNumbers);
+//         if (i>1){
+//         break;
+//    }
    
-}
-console.log(manyNumbers);
+// }
+// console.log(manyNumbers);
 
 
 
@@ -43,8 +43,8 @@ console.log(manyNumbers);
 // }
 
 
-const myArray=[1,2,5,6,8,9,2,7,5,9];
-var i=0;
+// const myArray=[1,2,5,6,8,9,2,7,5,9];
+// var i=0;
 
 // while(i<myArray.length){
 //     console.log(myArray[i]);
@@ -63,4 +63,56 @@ var i=0;
 //     console.log(i);
 
 // }
+
+
+///****array */
+
+// z = [1,2,3];
+// //1 2 3 
+//  z = 0;
+
+//  // push - d-daje na końcu
+//  // pop - usuwa ostatnie
+
+//  var a=[1,2,3];
+// a.pop();
+
+// console.log(a);
+
+// var d = [1,2,3];
+// d.unshift(4,5);
+// var r =d.shift();
+
+// console.log(d);
+// console.log(r);
+
+// var yourArray = [5,4,3,2,1];
+// yourArray.push(6);
+// yourArray.unshift(0);
+
+
+// console.log(yourArray);
+
+// yourArray.shift(0);
+// yourArray.pop(6);
+
+// console.log(yourArray);
+
+
+// const a = [1,2,3];
+// //const b = a;
+// const b = a.slice(0, a.length -1);
+// b.push(4);
+
+// console.log(a,b)
+
+//wyzej storzyl sie duplikat tablicy
+
+const a = [1,2,3, ['a', 'b','c']];
+
+const b = a.slice(0, a.length);
+
+
+b[3].push('d');
+console.log(a,b);
 
