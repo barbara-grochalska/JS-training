a440b1e55106126b9471809845d37ea4e7b6cca7
Barbara Grochalska <b.grochalska@gmail.com>
2018-09-29 09:19:34 +0200

    while

--- day3/main.js
+++ day3/main.js
@@ -19,12 +19,33 @@
 
 var manyNumbers = [];
 
-for(let i=0; i<10; i++){
-    const myNumbers = (Math.floor(Math.random() * 1000)+1);
-    manyNumbers.push(myNumbers);
+// for(let i=0; i<10; i++){
+//     const myNumbers = (Math.floor(Math.random() * 1000)+1);
+//     manyNumbers.push(myNumbers);
+// }
+
+// console.log(manyNumbers);
+
+// var i = 0;
+// var a = 0;
+
+// while(i < 10){
+//     i++;
+//     a += 2;
+//     console.log(i);
+// }
+
+
+const myArray=[1,2,5,6,8,9,2,7,5,9];
+var i=0;
+
+while(i<myArray.length){
+    console.log(myArray[i]);
+    i++;
 }
 
-console.log(manyNumbers);
+
+
 
 
 
